a9b7ab70cf857c810440156905a8c9650c28de7d
Samuel Correa <2354@holbertonschool.com>
2020-12-07 18:26:36 -0500

    Task 10 upload
    gitignore update and upload the tasks files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a linked list has a bucle
+ * @list: any position of the linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *start = list;
+
+	while (1)
+	{
+		list = list->next;
+		if (start == list)
+			return (1);
+		if (list == NULL)
+			return (0);
+	}
+}
