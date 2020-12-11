dc9a910084c93a4e21a8021adfdb18bbc83fefee
SamuelFCorrea <70902050+SamuelFCorrea@users.noreply.github.com>
2020-12-10 19:25:03 -0500

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import sys
 
-ac = len(sys.argv) - 1
-count = 1
+if __name__ == "__main__":
+    import sys
 
-if ac:
-    print("{} arguments:".format(ac))
-else:
-    print("{} arguments.".format(ac))
+    ac = len(sys.argv) - 1
+    count = 1
 
-for i in sys.argv[1:]:
-    print("{}: {}".format(count, i))
-    count += 1
+    if ac:
+        print("{} arguments:".format(ac))
+    else:
+        print("{} arguments.".format(ac))
+
+    for i in sys.argv[1:]:
+        print("{}: {}".format(count, i))
+        count += 1
