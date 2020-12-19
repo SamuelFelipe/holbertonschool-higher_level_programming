1077f3f0e40b0bf3fc6eb0b63496ff15d9ba0a35
SamuelFCorrea <70902050+SamuelFCorrea@users.noreply.github.com>
2020-12-19 14:32:25 -0500

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 
 
 def roman_to_int(roman_string):
+        if not roman_sting:
+                return None
         rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         int_val = 0
         for i in range(len(roman_string)):
