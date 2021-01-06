dd3aa9264d235eafe3b19b7b7f4ffd58c27f5ff5
Samuel Correa <2354@holbertonschool.com>
2021-01-06 12:28:47 -0500

    First commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            c += 1
+        print("")
+        return c
+    except IndexError:
+        print("")
+        return c
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    try:
+        for i in range(0, x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                c += 1
+            except (ValueError, TypeError):
+                continue
+        print("")
+        return c
+    except IndexError:
+        print("")
+        return c
