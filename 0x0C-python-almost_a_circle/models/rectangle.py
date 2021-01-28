5c9681c5863e7e4243e71fa0d28768cd00d8e909
SamuelFCorrea <70902050+SamuelFCorrea@users.noreply.github.com>
2021-01-28 17:07:33 -0500

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,9 @@ class Rectangle(Base):
     '''Class Rectangle'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''Initialize'''
+        '''Initialize the Rctangle class with the new pararameters:
+        width(required), height(required), x(optional), y(optional),
+        and use the id setter in the Base class'''
         super().__init__(id)
         self.width = width
         self.height = height
