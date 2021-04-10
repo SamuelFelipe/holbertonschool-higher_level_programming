fd48e4ffe57af574a6b9e6399ffca4059965974e
Samuel Felipe <2354@holbertonschool.com>
2021-04-09 21:16:21 -0500

    Update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,10 +6,11 @@ def find_peak(list_of_integers):
     '''Return the first peak'''
     if not list_of_integers:
         return None
-    elif len(list_of_integers) < 2:
-        return list_of_integers[0]
+
+    ret = list_of_integers[0]
 
     for i in range(1, len(list_of_integers) - 1):
         if list_of_integers[i - 1] < list_of_integers[i]:
             if list_of_integers[i + 1] < list_of_integers[i]:
                 return list_of_integers[i]
+    return ret
