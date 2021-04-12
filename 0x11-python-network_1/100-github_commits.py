ade5e6fee6906b8fce72c6e3edfeace9c5db7391
SamuelFelipe <70902050+SamuelFelipe@users.noreply.github.com>
2021-04-12 10:40:08 -0500

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,13 +2,11 @@
 
 '''Use the urllib to fetch https://intranet.hbtn.io/status'''
 
+import requests
+from sys import argv
 
-if __name__ == '__main__':
-    import requests
-    from sys import argv
+url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+req = requests.get(url, {'per_page': 10})
 
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
-    req = requests.get(url, {'per_page': 10})
-
-    for i in req.json():
-        print('{}: {}'.format(i['sha'], i['author']['login']))
+for i in req.json():
+    print('{}: {}'.format(i['sha'], i['author']['login']))
