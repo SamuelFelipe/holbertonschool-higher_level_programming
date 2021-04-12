4158cd16b3d64e68d81beab109319a6abfd0d662
Samuel Felipe <2354@holbertonschool.com>
2021-04-12 10:50:24 -0500

    Update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,11 +2,13 @@
 
 '''Use the urllib to fetch https://intranet.hbtn.io/status'''
 
-import requests
-from sys import argv
 
-url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
-req = requests.get(url, {'per_page': 10})
+if __name__ == '__main__':
+    import requests
+    from sys import argv
 
-for i in req.json():
-    print('{}: {}'.format(i['sha'], i['author']['login']))
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    req = requests.get(url, {'per_page': 10})
+
+    for i in req.json():
+        print('{}: {}'.format(i['sha'], i['author']['login']))
