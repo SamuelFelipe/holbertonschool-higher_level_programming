8e68011fbfff046a7ddb5a3aaafda4f0e414a7ed
SamuelFelipe <2354@holbertonschool.com>
2021-08-08 17:57:20 -0500

    Update

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -17,4 +17,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete')
+    cities = relationship('City', cascade='all, delete',
+                          back_populates='state')
